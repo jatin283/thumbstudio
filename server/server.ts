9d4432a1fc03fe627a88663208d4bf8ae649dc43
jatin283 <jatingoyal283648@gmail.com>
2026-03-01 17:48:51 +0530

    Feat: Created the user authentication APIs

--- server/server.ts
+++ server/server.ts
@@ -2,13 +2,37 @@ import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import AuthRouter from "./routes/AuthRoutes.js";
+
+declare module 'express-session' {
+    interface SessionData {
+        isLoggedIn: boolean;
+        userId: string;
+    }
+}
 
 await connectDB();
 
 const app = express();
 
 // Middleware
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173','http://localhost:3000'],
+    credentials: true
+}))
+
+app.use(session({
+    secret: process.env.SESSION_SECET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000*60*60*24*7}, //7 days
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI as string,
+        collectionName: 'sessions'
+    })
+}))
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
@@ -16,6 +40,8 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use('/api/auth', AuthRouter)
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
