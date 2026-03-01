d45e06b433d1fe73f0675d1aca059b3a3bbb9860
jatin283 <jatingoyal283648@gmail.com>
2026-03-01 16:28:42 +0530

    Feat: Created the backend server and connected the project with database

---
+++ server/server.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import connectDB from "./configs/db.js";
+
+await connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
